27d565f4b14ab0b6992f847fca75087478dfbc65
Hemanth Chowdary <achanta.hemanthchowdary@gmail.com>
2019-01-03 16:08:38 +0530

    Create Tic-Tac App

--- TicTacApp/ViewController.swift
+++ TicTacApp/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var activePlayer = 1 //Cross
+    var gameState = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
+    var gameActive : Bool = true
+    
+    @IBOutlet var playerWonLbl: UILabel!
+    
+    @IBOutlet var playAgain: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btnAction(_ sender: UIButton)
+    {
+        //print(sender.tag)
+        if gameState[sender.tag - 1] == 0
+        {
+            gameState[sender.tag - 1] = activePlayer
+            if activePlayer == 1
+            {
+                sender.setImage(UIImage.init(named:"cross.png"), for: .normal)
+                activePlayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage.init(named:"nought.png"), for: .normal)
+                activePlayer = 1
+            }
+        }
+        
+        for combination in winningCombination
+        {
+            //print("Game State:\(gameState)")
+            //print("Winning:\(winningCombination)")
+            //print(combination)
+            //print(gameState[combination[2]])
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameActive = false
+                if gameState[combination[0]] == 1
+                {
+                    playerWonLbl.text = "PLayer1 Has Won"
+                }
+                else
+                {
+                    playerWonLbl.text = "Player2 Has Won"
+                }
+                playAgain.isHidden = false
+                playerWonLbl.isHidden = false
+            }
+        }
+        gameActive = false
+        for i in gameState
+        {
+            if i == 0
+            {
+                gameActive = true
+                break
+            }
+        }
+        if gameActive == false
+        {
+            playerWonLbl.text = "It Was Draw"
+            playerWonLbl.isHidden = false
+            playAgain.isHidden = false
+        }
+    }
+    
+    @IBAction func palyAgainAction(_ sender: Any)
+    {
+        gameState = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameActive = true
+        activePlayer = 1
+        playAgain.isHidden = true
+        playerWonLbl.isHidden = true
+        for  i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: .normal)
+        }
+    }
 }
 
